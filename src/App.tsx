81dee818cf79808f7e6e3e3516ff96a78ac5c563
joshhhff <100181709@student.burnley.ac.uk>
2024-02-25 13:08:25 +0000

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import Services from './pages/Services';
