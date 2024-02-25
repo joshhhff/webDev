1b710c34d38e0c0935ad4ad1da3bf6771effbda3
joshhhff <100181709@student.burnley.ac.uk>
2024-02-25 13:34:39 +0000

    routing update services page

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -7,10 +7,10 @@ function Services() {
         <h1>SERVICES</h1>
         <section className={styles.sectionBlue}>
             <div className={styles.clickableImageContainer}>
-                <a href="/services/implementations"><img className={styles.clickableImage} src="../src/assets/Implementations Image.png" alt="" /></a>       
+                <a href="/webDev/#/services/implementations"><img className={styles.clickableImage} src="../src/assets/Implementations Image.png" alt="" /></a>       
             </div>
             <div className={styles.clickableImageContainer}>
-                <a href="/services/trainingandmentoring"><img className={styles.clickableImage} src="../src/assets/Training & Mentoring.png" alt="" /></a>  
+                <a href="/webDev/#/services/trainingandmentoring"><img className={styles.clickableImage} src="../src/assets/Training & Mentoring.png" alt="" /></a>  
             </div>
         </section>
         </div>
