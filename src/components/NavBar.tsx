162b40f98aa6b7725d9854248505ce9b17831f3a
joshhhff <100181709@student.burnley.ac.uk>
2024-02-26 14:05:18 +0000

    different nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router-dom';
+import { motion, AnimatePresence } from 'framer-motion';
 import { ScrollReveal } from 'reveal-on-scroll-react';
 import '../css/component-css/NavBar.css';
 
@@ -6,29 +7,46 @@ function NavBar() {
   return (
     <div className="navbar-container">
       <nav className="navbar">
-          <ScrollReveal.div animation='slide-in-left' className="logo">
-            <img src="./GoLiveExpertsLogo.jpg" />
-          </ScrollReveal.div>
+        <ScrollReveal.div animation="slide-in-left" className="logo">
+          <img src="./GoLiveExpertsLogo.jpg" alt="Logo" />
+        </ScrollReveal.div>
 
-        <ScrollReveal.div animation='slide-in-right'>
-          <ul className="nav-list">
-            <li>
-              <NavLink to="/" activeClassName="active">HOME</NavLink>
-            </li>
-            <li>
-              <NavLink to="/services" activeClassName="active">SERVICES</NavLink>
-            </li>
-            <li>
-              <NavLink to="/aboutus" activeClassName="active">ABOUT US</NavLink>
-            </li>
-            <li>
-              <NavLink to="/jointheteam" activeClassName="active">JOIN THE TEAM</NavLink>
-            </li>
-            <li>
-              <NavLink to="/contactus" activeClassName="active">CONTACT US</NavLink>
-            </li>
-          </ul>
+        <AnimatePresence>
+          <ScrollReveal.div animation="slide-in-right">
+            <motion.ul
+              initial={{ height: 0, opacity: 0 }}
+              animate={{ height: 'auto', opacity: 1 }}
+              exit={{ height: 0, opacity: 0 }}
+              className="nav-list"
+            >
+              <li>
+                <NavLink to="/" activeClassName="active">
+                  HOME
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/services" activeClassName="active">
+                  SERVICES
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/aboutus" activeClassName="active">
+                  ABOUT US
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/jointheteam" activeClassName="active">
+                  JOIN THE TEAM
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/contactus" activeClassName="active">
+                  CONTACT US
+                </NavLink>
+              </li>
+            </motion.ul>
           </ScrollReveal.div>
+        </AnimatePresence>
       </nav>
     </div>
   );
