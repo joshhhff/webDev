63fb2e9076877af1849dac92151948ebc0ce9009
joshhhff <100181709@student.burnley.ac.uk>
2024-02-26 12:31:01 +0000

    commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,11 @@
 import styles from '../css/page-css/Home.module.css';
 import InfoCard from '../components/InfoCard';
 import Button from '../components/Button';
-import NavBar from '../components/NavBar';
 import { ScrollReveal } from 'reveal-on-scroll-react';
 
 function Home() {
   return (
     <>
-    <NavBar />
     <div className={styles.mainBody}>
         <ScrollReveal.div>
         <section className={styles.sectionBlue}>
