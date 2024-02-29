42ab9f31b9b584d2a60d3f315464bd9438e3c29b
joshhhff <100181709@student.burnley.ac.uk>
2024-02-29 21:28:09 +0000

    background color change fix

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,13 +1,14 @@
 import { motion } from 'framer-motion';
 import NavBar from '../components/NavBar';
 import '../css/page-css/Pages.css';
+import styles from '../css/page-css/JoinTheTeam.module.css' //fixing background color on services page to be white on top of background colour. create css file for all pages like these to share (instead of seperate files)
 
 function Services() {
   return (
     <>
     <NavBar animate={false} />
     <div className="background">
-        <div className="mainBody">
+        <div className={styles.mainBody}>
         <h1 className="pageHeader">SERVICES</h1>
         <motion.section className="sectionBlue" initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1}}>
             <motion.div className="clickableImageContainer" whileHover={{borderRadius: "3%"}} whileTap={{scale: 0.9}}>
