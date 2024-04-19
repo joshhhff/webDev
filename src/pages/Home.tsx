e66fb0bd6175c343efcb1383a8341918370a3680
joshhhff <100181709@student.burnley.ac.uk>
2024-04-19 11:50:41 +0100

    test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,7 +25,7 @@ function Home() {
         if (i < text.length) {
             console.log('current i value', i)
             console.log('adding character', text.charAt(i))
-            setState(prevText => prevText + text.charAt(i));
+            setState(prevText => prevText + text.charAt(1));
             console.log(title)
             i++;
             typingTimer = setTimeout(() => typeText(text, setState, speed, isTitle, isMessage), speed); //call the function again if all of text has not been "typed"
